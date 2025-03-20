b343d8aa7f79debdafffb1753f8c9a366e748526
aberton228 <aberton.3@mail.ru>
2025-03-20 19:05:00 +0300

    poisonTest

--- game/scenes/test scene/potion_aoe.gd
+++ game/scenes/test scene/potion_aoe.gd
@@ -1,9 +1,11 @@
 extends Node2D
-
+@export var duartion = 1.0
+@onready var reload_timer: Timer = $Timer
 func _ready():
 	# Подключаем сигнал таймера к функции
+	reload_timer.wait_time = duartion
+	reload_timer.start()
 	$Timer.timeout.connect(_on_Timer_timeout)
-
 func _on_Timer_timeout():
 	# Удаляем объект или делаем его невидимым
 	queue_free()  # Удаляет объект из сцены
